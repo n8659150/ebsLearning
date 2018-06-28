43797b4c06f97d2bed4524579482961f06d5f694
n8659150 <jackli900421@hotmail.com>
2018-06-28 22:11:09 +0800

    整理项目文件夹，删除冗余文件

--- video_source_config/purchases.js
+++
@@ -1,4 +0,0 @@
-export const purchases_data = [
-    { "title": "BOE001_How_to_create_imported_PO", "video_url": "/BOE001.wmv", "length": "1:09" },
-    { "title": "BOE002_How_to_create_BOE", "video_url": "/BOE002.wmv", "length": "1:09" }
-]
